1bc74ee816f6a6f08556df3896cf6b117b314608
anamspe <anamspe.fr@gmail.com>
2025-07-03 16:06:04 -0700

    Minor styling adjustments

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -3,8 +3,6 @@ function FileUpload({ onFileLoaded }) {
     const file = e.target.files?.[0];
     if (file) {
       onFileLoaded(file);
-    } else {
-      console.error("No file selected");
     }
 
     const reader = new FileReader();
@@ -16,12 +14,12 @@ function FileUpload({ onFileLoaded }) {
   };
 
   return (
-    <div className="mb-4">
+    <div className="m-4 flex flex-col self-center">
       <input
         type="file"
         accept=".srt,.vtt,.txt, .csv"
         onChange={handleFileChange}
-        className="block w-full text-sm text-gray-700 underline decoration-sky-500 file:mr-4 file:py-2 file:px-4 file:rounded-full File:border-0 file:text-sm file:font-semibold file:bg-blue-100 file:text-blue-700 hover:file:bg-blue-200 file:cursor-pointer file:transition-colors file:duration-300 file:ease-in-out file:hover:bg-blue-200"
+        className="block w-full text-sm text-gray-700 underline decoration-sky-600 file:mr-4 file:py-2 file:px-4 file:rounded-full File:border-0 file:text-sm file:font-semibold file:bg-blue-100 file:text-blue-700 hover:file:bg-blue-200 file:cursor-pointer file:transition-colors file:duration-300 file:ease-in-out"
       />
       <p className="text-xs text-gray-500 mt-2">
         Supported formats: .srt, .vtt, .txt, .csv
