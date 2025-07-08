5dc8db29cca39f4d13319574f419409f6ed88776
anamspe <anamspe.fr@gmail.com>
2025-07-07 22:36:21 -0700

    Add parsing logic

--- src/utils/parseSRT.js
+++ src/utils/parseSRT.js
@@ -3,5 +3,24 @@
 export const parseSRT = (text) => {
   const blocks = text.split(/\r?\n\r?\n/);
   console.log("Blocks found:", blocks.length);
+
+  const subtitles = blocks.map((block) => {
+    const lines = block.split(/\r?\n/).filter(Boolean); // splits block by line and ignores empty lines
+
+    if (lines.length < 3) return null; //skip blocks with no caption text
+
+    const id = parseInt(lines[0]);
+    const [start, end] = lines[1].split(" --> ");
+    const subtitleText = lines.slice(2).join("\n"); // handle multiline subtitles
+
+    return {
+      id,
+      start,
+      end,
+      text: subtitleText,
+    };
+  }).filter(Boolean); // remove nulls from the final array if any
+
+  console.log("Parsed subtitles:", subtitles)
 }
 
